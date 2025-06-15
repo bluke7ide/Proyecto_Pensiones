ec57ada29c0c8b5be4559db70cd12fbd295c847c
bluke7ide <luisfernandoamey@gmail.com>
2025-06-15 15:53:05 -0600

    chore: estocastico

--- cod/curva_dens.R
+++ cod/curva_dens.R
@@ -13,14 +13,16 @@ curva_dens <- function(){
   curva_dens <- edades[edades$considerar,] %>% 
     select(-name, - inicio, -considerar) %>% 
     group_by(edad) %>% 
-    summarise(media = mean(value))
+    summarise(media = mean(value),
+              std = var(value)^0.5)
   dens_lm <- lm(media ~ log(edad), curva_dens[10:40,]) # se fitea log
-  
+  std_lm <- lm(std ~ log(edad),  curva_dens[5:40,] )
   curva_dens[1:96,1] <- 20:115
   curva_dens[41:96,2] <- predict.lm(dens_lm, data.frame(edad = 60:115))
+  curva_dens[41:96,3] <- predict.lm(std_lm, data.frame(edad = 60:115))
   return(curva_dens)
 }
-
+curva_densidad <- curva_dens()
 cot_prop <- function(){
   # Tomando el valor presente
   vp_cot <- sapply(1:ncol(cotizaciones), function(x) t(cotizaciones[,x]) * niveles[[x]])
--- cod/curva_salarial.R
+++ cod/curva_salarial.R
@@ -1,47 +1,44 @@
-IPC <- read_excel("data/ipc_inec.xlsx", skip = 3)
-BD_Cotizantes <- read_excel("data/BD_Cotizantes.xlsx")
-colnames(BD_Cotizantes)[4:ncol(BD_Cotizantes)] <- as.character(
-  as.Date(as.numeric(colnames(BD_Cotizantes)[4:ncol(BD_Cotizantes)]),
-          origin = "1899-12-30"))
-
-# ---------------------------------
-curva_salarial <- BD_Cotizantes %>%
-  select(-c(ID, Sexo)) %>%
-  pivot_longer(cols = c(-Fec.Nac),
-               names_to = "Fecha",
-               values_to = "Salario") %>%
-  filter(Salario > 0) %>%
-  mutate(
-    Fecha = ymd(Fecha),
-    Fec.Nac = ymd(Fec.Nac),
-    Edad = floor(as.numeric(difftime(
-      Fecha, Fec.Nac, units = "days"
-    )) / 365.25),
-    Fecha_num = match(Fecha, sort(unique(Fecha))),
-    Factor_ajuste = 110.39017 / IPC$Nivel[228 + Fecha_num],
-    Salario_ajustado = Salario * Factor_ajuste
-  ) %>%
-  group_by(Edad) %>%
-  summarise(Salario_promedio = mean(Salario_ajustado)) %>%
-  ungroup() %>%
-  mutate(
-    Salario_promedio = ifelse(Edad > 70,
-                              Salario_promedio[Edad == 70],
-                              Salario_promedio)
-    ) %>% 
-  complete(Edad = 20:115) %>% 
-  mutate(
-    Salario_promedio = ifelse(is.na(Salario_promedio), 
-                              Salario_promedio[Edad == 70], 
-                              Salario_promedio),
-    s_x = lead(Salario_promedio) / Salario_promedio,
-    s_x = ifelse(is.na(s_x), 1, s_x)
-  )
-curva_salarial$s_x <- c(curva_salarial$s_x[1:40], smooth(curva_salarial$s_x[41:96]))
-# ggplot(curva_salarial, aes(x = Edad, y = cumprod(s_x))) +
-#   geom_point(alpha = 0.5) +
-#   geom_smooth(method = "gam",
-#               formula = y ~ s(x, bs = "cs"),
-#               se = FALSE,
-#               color = "steelblue") +
-#   theme_minimal()
+curva_sal <- function(){
+  IPC <- read_excel("data/ipc_inec.xlsx", skip = 3)
+  BD_Cotizantes <- read_excel("data/BD_Cotizantes.xlsx")
+  colnames(BD_Cotizantes)[4:ncol(BD_Cotizantes)] <- as.character(
+    as.Date(as.numeric(colnames(BD_Cotizantes)[4:ncol(BD_Cotizantes)]),
+            origin = "1899-12-30"))
+  
+  # ---------------------------------
+  curva_salarial <- BD_Cotizantes %>%
+    select(-c(ID, Sexo)) %>%
+    pivot_longer(cols = c(-Fec.Nac),
+                 names_to = "Fecha",
+                 values_to = "Salario") %>%
+    filter(Salario > 0) %>%
+    mutate(
+      Fecha = ymd(Fecha),
+      Fec.Nac = ymd(Fec.Nac),
+      Edad = floor(as.numeric(difftime(
+        Fecha, Fec.Nac, units = "days"
+      )) / 365.25),
+      Fecha_num = match(Fecha, sort(unique(Fecha))),
+      Factor_ajuste = 110.39017 / IPC$Nivel[228 + Fecha_num],
+      Salario_ajustado = Salario * Factor_ajuste
+    ) %>%
+    group_by(Edad) %>%
+    summarise(Salario_promedio = mean(Salario_ajustado),
+              std = var(Salario_ajustado)^0.5) %>%
+    ungroup()  %>% 
+    complete(Edad = 20:115) %>% 
+    mutate(
+      Salario_promedio = ifelse(is.na(Salario_promedio), 
+                                Salario_promedio[Edad == 70], 
+                                Salario_promedio),
+      s_x = lead(Salario_promedio) / Salario_promedio,
+      s_x = ifelse(is.na(s_x), 1, s_x),
+      std = std/Salario_promedio
+    )
+    mean_lm <- lm(s_x ~ log(Edad), curva_salarial[15:40,])
+    std_lm <- lm(std ~ log(Edad), curva_salarial[15:40,])
+    curva_salarial[41:96,4] <- predict.lm(mean_lm, data.frame(Edad = 60:115))
+    curva_salarial[41:96,3] <- predict.lm(std_lm, data.frame(Edad = 60:115))
+    return(curva_salarial)
+}
+curva_salarial <- curva_sal()
